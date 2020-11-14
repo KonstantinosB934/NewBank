5cd7df18e7171f5965bcc29c16a4412ed23bf4e7
Siyanda-mkhize <31338432+Siyanda-mkhize@users.noreply.github.com>
2020-11-15 01:13:46 +0200

    Add files via upload
    Implement functions for users to update passwords and create a new bank account.

--- newbank/server/NewBank.java
+++ newbank/server/NewBank.java
@@ -13,15 +13,18 @@ public class NewBank {
 	}
 	
 	private void addTestData() {
+//		Customer bhagy = new Customer();
 		Customer bhagy = new Customer("123");
 		bhagy.addAccount(new Account("Main", 1000.0));
+		bhagy.addAccount(new Account("Savings", 2000.0));
 		customers.put("Bhagy", bhagy);
+
 		
-		Customer christina = new Customer("234");
+		Customer christina = new Customer();
 		christina.addAccount(new Account("Savings", 1500.0));
 		customers.put("Christina", christina);
-		
-		Customer john = new Customer("456");
+
+		Customer john = new Customer();
 		john.addAccount(new Account("Checking", 250.0));
 		customers.put("John", john);
 	}
@@ -29,11 +32,13 @@ public class NewBank {
 	public static NewBank getBank() {
 		return bank;
 	}
+
 	
 	public synchronized CustomerID checkLogInDetails(String userName, String password) {
 		if(customers.containsKey(userName)) {
+//			return new CustomerID(userName);
 			Customer customer = customers.get(userName);
-			if(customer.getPassword().equals(password)) {
+			if(customer.getPassword().equals(password)){
 				return new CustomerID(userName);
 			}
 		}
@@ -43,8 +48,23 @@ public class NewBank {
 	// commands from the NewBank customer are processed in this method
 	public synchronized String processRequest(CustomerID customer, String request) {
 		if(customers.containsKey(customer.getKey())) {
+
+			if(request.startsWith("MOVE")) {
+				return moveMoney(customer, request);
+			}
+
+			if(request.startsWith("UPDATE")){
+				return update(customer, request);
+			}
+
+			if(request.startsWith("NEWACCOUNT")){
+				return createAcc(customer, request);
+			}
+
+
 			switch(request) {
 			case "SHOWMYACCOUNTS" : return showMyAccounts(customer);
+
 			default : return "FAIL";
 			}
 		}
@@ -53,6 +73,36 @@ public class NewBank {
 	
 	private String showMyAccounts(CustomerID customer) {
 		return (customers.get(customer.getKey())).accountsToString();
+	}
+
+	private String moveMoney(CustomerID customer, String request) {
+		String[] movecommand = request.split(" ");
+
+		String From = movecommand[2];
+		String To = movecommand[3];
+		Double Amount = Double.parseDouble(movecommand[1]);
+
+		return (customers.get(customer.getKey())).moveMoney(From, To, Amount);
+	}
+
+	private String update(CustomerID customer, String request){
+		String[] updateCommand = request.split(" ");
+
+		String newPassword = updateCommand[1];
+		String confirmPassword = updateCommand[2];
+
+
+		return (customers.get(customer.getKey())).update(newPassword,confirmPassword);
+	}
+
+	private String createAcc(CustomerID customer, String request){
+		String[] createCommand = request.split(" ");
+
+		String accountName = createCommand[1];
+		double openingBalance = Double.parseDouble(createCommand[2]);
+
+
+		return(customers.get(customer.getKey())).addAccount(new Account (accountName, openingBalance));
 	}
 
 }
